6437f0a8b0579097edaa7a4e2d8a1240c2e9c11a
deozone11-beep <deozone11@gmail.com>
2026-07-07 10:16:35 +0530

    1st commit

---
+++ backend/expense-tracker/src/main/java/com/expense_tracker/expense_tracker/response/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.expense_tracker.expense_tracker.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+
+    private String message;
+
+    private T data;
+
+}
